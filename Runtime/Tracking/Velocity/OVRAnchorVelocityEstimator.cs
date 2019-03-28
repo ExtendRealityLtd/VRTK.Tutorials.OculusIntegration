446dbc67ad763f4df9647639ef46b60cdd7b5a76
Harvey Ball <me@harveyball.com>
2019-03-28 14:48:13 +0000

    feat(Velocity): add public field to allow injecting tracked anchor
    A public field has been added to allow injecting a GameObject that
    will represent the OVRCameraRig anchor to track the velocities on.
    
    This is in line with Step 5 of the tutorial.

--- Runtime/Tracking/Velocity/OVRAnchorVelocityEstimator.cs
+++ Runtime/Tracking/Velocity/OVRAnchorVelocityEstimator.cs
@@ -5,6 +5,9 @@
 
     public class OVRAnchorVelocityEstimator : VelocityTracker
     {
+        [Tooltip("The GameObject anchor from the OVRCameraRig to track velocity for.")]
+        public GameObject trackedGameObject;
+
         protected override Vector3 DoGetAngularVelocity()
         {
             throw new System.NotImplementedException();

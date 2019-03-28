cab5f94abd671797672679bdc4e1483b6f96c069
Harvey Ball <me@harveyball.com>
2019-03-28 16:49:08 +0000

    feat(Velocity): add angular velocity data retrieval method
    The DoGetAngularVelocity method has been updated to return angular
    velocities for the headset and both left, right controllers in line
    with Step 8 of the tutorial.

--- Runtime/Tracking/Velocity/OVRAnchorVelocityEstimator.cs
+++ Runtime/Tracking/Velocity/OVRAnchorVelocityEstimator.cs
@@ -10,7 +10,16 @@
 
         protected override Vector3 DoGetAngularVelocity()
         {
-            throw new System.NotImplementedException();
+            switch (trackedGameObject.name)
+            {
+                case "CenterEyeAnchor":
+                    return OVRManager.isHmdPresent ? OVRPlugin.GetNodeAngularVelocity(OVRPlugin.Node.EyeCenter, OVRPlugin.Step.Render).FromFlippedZVector3f() : Vector3.zero;
+                case "LeftHandAnchor":
+                    return OVRInput.GetLocalControllerAngularVelocity(OVRInput.Controller.LTouch);
+                case "RightHandAnchor":
+                    return OVRInput.GetLocalControllerAngularVelocity(OVRInput.Controller.RTouch);
+            }
+            return Vector3.zero;
         }
 
         protected override Vector3 DoGetVelocity()

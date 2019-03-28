56866a14bbd283547a3a90867dd77ad6f9173597
Harvey Ball <me@harveyball.com>
2019-03-28 14:22:47 +0000

    feat(Velocity): extend Zinnia Velocity Tracker and provide definitions
    The class now extends the Zinnia Velocity Tracker and provides default
    implementations of the required contract definitions.
    
    The component won't function correctly still because the default method
    implementations simply throw exceptions at the moment.
    
    This is in line with Step 4 of the tutorial.

--- Runtime/Tracking/Velocity/OVRAnchorVelocityEstimator.cs
+++ Runtime/Tracking/Velocity/OVRAnchorVelocityEstimator.cs
@@ -1,8 +1,18 @@
 ﻿namespace OculusIntegrationForVRTK.Tracking.Velocity
 {
     using UnityEngine;
+    using Zinnia.Tracking.Velocity;
 
-    public class OVRAnchorVelocityEstimator : MonoBehaviour
+    public class OVRAnchorVelocityEstimator : VelocityTracker
     {
+        protected override Vector3 DoGetAngularVelocity()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override Vector3 DoGetVelocity()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
